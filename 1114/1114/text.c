37c6f3be46fb481fcb2474cdbceb948680bc59ee
fangzy1224 <2734895406@qq.com>
2020-11-14 22:47:56 +0800

    null

---
+++ 1114/1114/text.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+//两个int（32位）整数m和n的二进制表达中，有多少个位(bit)不同？ 
+
+
+int main(){
+	int a = 10;
+	printf("奇数位：");
+	while (a){
+		for (int i = 1; i < a; i++){
+			printf("%d ", (a % 2) & 1);
+			a = a >> 2;
+		}
+		
+	}
+	
+	system("pause");
+	return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*int main(){
+	int n = 10;
+	int count = 0;
+	while (n){
+		if (n & 1 == 1){
+			count++;
+		}
+		n = n >> 1;
+	}
+	printf("%d\n", count);
+	system("pause");
+	return 0;
+}*/
+
+
+
+
+
+
+
+
+
+
+/*//求Sn = a + aa + aaa + aaaa + aaaaa的前5项之和，其中a是一个数字，
+//例如：2 + 22 + 222 + 2222 + 22222
+int sn(int n){
+	int sum = 0;
+	int tmp = 0;
+	for (int i = 1; i <=5; i++){
+		tmp = tmp * 10 + n;
+		sum += tmp;
+	}
+	return sum;
+}
+int main(){
+
+	printf("%d\n", sn(5));
+    system("pause");
+	return 0;
+}
+//写一个函数，可以逆序一个字符串的内容。
+void reverse(char *str,int n){
+	char*left = str;
+	char* right = str + n - 1;
+	char tmp = 0;
+	while (*left < *right){
+		tmp = *left;
+		*left = *right;
+		*right = tmp;
+		++*left;
+		--*right;
+	}
+
+}
+
+int main(){
+
+	char str[] = "study";
+	int size = strlen(str);
+	reverse(str, size);
+	printf("%s ", str);
+	system("pause");
+	return 0;
+
+}*/
+//写一个函数打印arr数组的内容，不使用数组下标，使用指针。arr是一个整形一维数组。
+/*int print(int *arr,int n){
+
+	for (int i = 0; i < n; i++){
+		printf("%d ", *arr + i);
+	}
+}
+int main(){
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+	print(arr, 7);
+	system("pause");
+}*/
+
--- 2020_11_9/2020_11_9/text.c
+++ 2020_11_9/2020_11_9/text.c
@@ -1,14 +1,40 @@
 #include<stdio.h>
-//写一个代码打印1-100之间所有3的倍数的数字
-  int main(){
-	  for (int i = 1; i <= 100; i++){
-		  if (i % 3 == 0){
-			  printf("%d ", i);
+#include<math.h>
+#pragma warning(disable:4996)
+//编写代码在一个整形有序数组中查找具体的某个数
+
+//要求：找到了就打印数字所在的下标，找不到则输出：找不到。
+    int main(){
+	  int a = 0;
+	  int arr[] = { 2, 3, 5, 8, 9, 23, 45 };
+	  printf("请输入你要查找的元素：");
+	  scanf("%d", &a);
+	  
+	  int l = 0;
+	  int r = sizeof(arr) / sizeof(arr[0]) - 1;
+	
+	  while (l < r){
+		  int m = l +( r-l) / 2;
+		  if (a < arr[m]){
+			  r = m - 1;
+                    }
+		  else if (a > arr[m])
+		  {
+			  l = m + 1;
+		  }
+		  else{
+			  printf("找到了，下标为%d\n", m);
 		  }
 	  }
+	  
+		  if (l >= r)
+			  printf("找不到\n");
+		  
+	 system("pause");
+	  return 0;
+  }
+  
 
 
 
-	system("pause");
-	return 0;
-            }
+	
